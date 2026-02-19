d81fff595978974f3f7d0d4acad31607726841eb
thorxenon <pedrolucasrangel1@hotmail.com>
2026-02-19 16:06:13 -0300

    test: add unit tests for findOne, likeTweet, unlikeTweet, and getAnswersFromTweet methods in TweetsController

--- src/tweets/tweets.controller.spec.ts
+++ src/tweets/tweets.controller.spec.ts
@@ -22,6 +22,8 @@ describe('TweetsController', () => {
   };
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const moduleBuilder = Test.createTestingModule({
       imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
       controllers: [TweetsController],
@@ -54,6 +56,53 @@ describe('TweetsController', () => {
 
     expect(tweetsServiceMock.create).toHaveBeenCalledWith(createTweetDto, 'test-user');
     expect(result).toEqual(expected);
+  });
+
+  it('GET /tweets/:id should return a tweet by id', async () => {
+    tweetsServiceMock.findOne.mockResolvedValue([]);
+    const req = { user: { slug: 'test-user' } } as any;
+    tweetsServiceMock.findOne.mockResolvedValue({ id: 1, body: 'This is a test tweet' });
+    const result = await controller.findOne('1', req);
+
+    expect(tweetsServiceMock.findOne).toHaveBeenCalledWith(1, 'test-user');
+    expect(result).toEqual({ id: 1, body: 'This is a test tweet' });
+  });
+
+  it('GET /tweets/:id/like should like tweet if not liked yet', async () => {
+    likesServiceMock.verifyWhetherTweetIsLikedByUser.mockResolvedValue(false);
+    likesServiceMock.likeTweet.mockResolvedValue({  id: 1, tweetId: 1, userSlug: 'test-user' });
+    const result = await controller.likeTweet('1', { user: { slug: 'test-user' } } as any);
+
+    expect(likesServiceMock.verifyWhetherTweetIsLikedByUser).toHaveBeenCalledWith(1, 'test-user');
+    expect(likesServiceMock.likeTweet).toHaveBeenCalledWith(1, 'test-user');
+    expect(likesServiceMock.unlikeTweet).not.toHaveBeenCalled();
+    expect(result).toEqual({  id: 1, tweetId: 1, userSlug: 'test-user' });
+  });
+
+  it('GET /tweets/:id/like should unlike tweet when already liked', async () => {
+    likesServiceMock.verifyWhetherTweetIsLikedByUser.mockResolvedValue(true);
+    likesServiceMock.unlikeTweet.mockResolvedValue(undefined);
+    const req = { user: { slug: 'test-user' } } as any;
+    const result = await controller.likeTweet('1', req);
+
+    expect(likesServiceMock.verifyWhetherTweetIsLikedByUser).toHaveBeenCalledWith(1, 'test-user');
+    expect(likesServiceMock.unlikeTweet).toHaveBeenCalledWith(1, 'test-user');
+    expect(likesServiceMock.likeTweet).not.toHaveBeenCalled();
+    expect(result).toBeUndefined();
+  });
+
+  it('GET /tweets/:id/answers should return answers from a tweet', async() =>{
+    tweetsServiceMock.getAnswersFromTweet.mockResolvedValue([
+      { id: 2, body: 'This is an answer to the tweet', replyToId: 1 },
+      { id: 3, body: 'This is an answer to the tweet 1', replyToId: 1 },
+    ]);
+    const result = await controller.getAnswersFromTweet('1');
+
+    expect(tweetsServiceMock.getAnswersFromTweet).toHaveBeenCalledWith(1);
+    expect(result).toEqual([
+      { id: 2, body: 'This is an answer to the tweet', replyToId: 1 },
+      { id: 3, body: 'This is an answer to the tweet 1', replyToId: 1 },
+    ]);
   });
 
   it('should be defined', () => {
